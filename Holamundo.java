074dde442a464954b2ba774b12dd3a628a2c6b97
pedrojgp82 <pedrojgp82@gmail.com>
2020-12-11 17:14:33 -0800

    Programa Holamundo en java tercera modificacion

--- Holamundo.java
+++ Holamundo.java
@@ -1,7 +1,7 @@
 public class Holamundo {
 
 	public static void main(String[] args) {		
-		System.out.println("Hola Mundo modificacion1");
+		System.out.println("Hola Mundo modificacion2");
 	}
 
 }

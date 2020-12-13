78a7da58ed0b0aab62ea28b4a479a56b4d2574ee
pedrojgp82 <pedrojgp82@gmail.com>
2020-12-13 00:28:16 -0800

    Un nuevo comentario para ver diff

--- Holamundo.java
+++ Holamundo.java
@@ -2,6 +2,7 @@ public class Holamundo {
 
 	public static void main(String[] args) {		
 		System.out.println("Hola Mundo cuarta modificacion desde inicial");
+		System.out.println("Prueba de diff");
 	}
 
 }
